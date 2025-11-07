9b6905e5d114b4d91055439b635439aec9830c0d
Bernard Krehula <krehula.bernard456@gmail.com>
2025-11-07 21:24:38 +0100

    postData created

---
+++ my-app/src/data/postData.ts
@@ -0,0 +1,181 @@
+export const postData = {
+    "status": "ok",
+    "posts": [
+        {
+            "post_id": "f5359fa5-d2f2-4f9b-984c-2e7157e63209",
+            "user_id": "ec7bb1a4-464d-4a32-84fe-dd31c6a88a6f",
+            "text": "Climate change refers to long-term alterations in global weather patterns and temperature, primarily driven by human activities like burning fossil fuels and deforestation. Its consequences include rising temperatures, extreme weather events, and sea-level rise, posing significant environmental and societal challenges. Urgent action through sustainable practices and international cooperation is crucial to mitigate the impacts of climate change and protect our planet's future.",
+            "image": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/4f918bc4-c0d9-4e7d-9c91-387316234058-mountain.jpeg",
+            "audio": null,
+            "comments": 3,
+            "likes": 2,
+            "created_at": "2023-09-14T17:44:03.772Z",
+            "user": {
+                "username": "constelic",
+                "full_name": "John Constelic",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/image+(2).png"
+            },
+            "liked": false
+        },
+        {
+            "post_id": "ba6b306a-c35a-4d3b-ba23-ff0a2928c784",
+            "user_id": "156653f9-5363-43a3-82ac-3426c5bc37cd",
+            "text": "The bird, with its graceful flight and vibrant plumage, is a symbol of freedom and natural beauty in the animal kingdom. Its melodious songs and diverse species enrich ecosystems around the world, making it a cherished and integral part of our environment.",
+            "image": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/4ed50e63-c907-4485-942b-eaca409c4264-bird.jpeg",
+            "audio": null,
+            "comments": 1,
+            "likes": 1,
+            "created_at": "2023-09-14T17:59:51.700Z",
+            "user": {
+                "username": "annedoe",
+                "full_name": "Anne Doe",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/woman.jpg"
+            },
+            "liked": false
+        },
+        {
+            "post_id": "c12398b9-be35-40a8-a5e7-fd1c4ea35c56",
+            "user_id": "1c695c08-ecfc-4b7b-b253-1efa04ceea92",
+            "text": "Nestled amidst lush landscapes, the tranquil lake reflects the ever-changing colors of the sky, offering a serene and picturesque retreat for both humans and wildlife. Its clear waters provide a habitat for aquatic life and a serene setting for recreational activities, making it a cherished natural gem in the heart of nature.",
+            "image": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/0baaac37-b19e-450e-acbb-16087041b9c6-lake.jpg",
+            "audio": null,
+            "comments": 0,
+            "likes": 1,
+            "created_at": "2023-09-14T18:02:28.353Z",
+            "user": {
+                "username": "leesenior",
+                "full_name": "Lee Senior",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/person-1.jpg"
+            },
+            "liked": true
+        },
+        {
+            "post_id": "065c5b5c-17bc-4f52-bfa0-e394824ccfef",
+            "user_id": "1c695c08-ecfc-4b7b-b253-1efa04ceea92",
+            "text": "Hiking, an exhilarating outdoor adventure, allows individuals to connect with nature while exploring diverse landscapes and challenging terrains. It provides an opportunity to escape the hustle and bustle of daily life, offering physical exercise, mental rejuvenation, and a deeper appreciation for the natural world.",
+            "image": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/3a99db9a-a4ca-46dd-8ddb-8ca40da61fc6-hiking.jpg",
+            "audio": null,
+            "comments": 0,
+            "likes": 1,
+            "created_at": "2023-09-14T18:02:59.514Z",
+            "user": {
+                "username": "leesenior",
+                "full_name": "Lee Senior",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/person-1.jpg"
+            },
+            "liked": true
+        },
+        {
+            "post_id": "ebbbe344-12a9-4c54-8fde-31d7ab93794f",
+            "user_id": "156653f9-5363-43a3-82ac-3426c5bc37cd",
+            "text": "Female power represents the strength, resilience, and influence that women possess in various aspects of life, from leadership roles to advocating for social change. It embodies the capacity to break barriers, challenge stereotypes, and inspire future generations through determination and accomplishments. Recognizing and empowering female voices and contributions fosters a more inclusive and equitable society for all.",
+            "image": null,
+            "audio": null,
+            "comments": 0,
+            "likes": 2,
+            "created_at": "2023-09-14T18:03:48.742Z",
+            "user": {
+                "username": "annedoe",
+                "full_name": "Anne Doe",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/woman.jpg"
+            },
+            "liked": true
+        },
+        {
+            "post_id": "e048e889-96fb-4ef1-9ff5-7baaee65bd23",
+            "user_id": "ec7bb1a4-464d-4a32-84fe-dd31c6a88a6f",
+            "text": "Welcome to Costel Network!!!",
+            "image": null,
+            "audio": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/54f0f6d5-f53c-47f7-a1aa-bdb731e80597-blob",
+            "comments": 0,
+            "likes": 1,
+            "created_at": "2023-09-14T18:30:45.595Z",
+            "user": {
+                "username": "constelic",
+                "full_name": "John Constelic",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/image+(2).png"
+            },
+            "liked": true
+        },
+        {
+            "post_id": "26d462a2-7a38-45f4-9b82-6ac0b6500c8c",
+            "user_id": "0a48a2b3-f660-4b20-a261-c9ef15c286f7",
+            "text": "Zarila kandze Andjela, mrtvo hladnoooo ooooooooo ...gasi",
+            "image": null,
+            "audio": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/c34579c2-def9-436e-bc3b-c8b0a1b27a94-recording.wav",
+            "comments": 0,
+            "likes": 1,
+            "created_at": "2024-08-17T15:53:24.182Z",
+            "user": {
+                "username": "nemanja_malesija",
+                "full_name": "Nemanja Malesija",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/nemanja_malesija.jpeg"
+            },
+            "liked": true
+        },
+        {
+            "post_id": "bd7b2edb-c015-44d6-8ef8-8c50d96d351d",
+            "user_id": "0a48a2b3-f660-4b20-a261-c9ef15c286f7",
+            "text": "Mario",
+            "image": null,
+            "audio": null,
+            "comments": 1,
+            "likes": 0,
+            "created_at": "2024-08-17T16:10:05.609Z",
+            "user": {
+                "username": "nemanja_malesija",
+                "full_name": "Nemanja Malesija",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/nemanja_malesija.jpeg"
+            },
+            "liked": false
+        },
+        {
+            "post_id": "c32ab40d-9b92-4278-8def-27c3512faeba",
+            "user_id": "0a48a2b3-f660-4b20-a261-c9ef15c286f7",
+            "text": "Zdravo",
+            "image": null,
+            "audio": null,
+            "comments": 0,
+            "likes": 0,
+            "created_at": "2025-09-30T20:12:32.624Z",
+            "user": {
+                "username": "nemanja_malesija",
+                "full_name": "Nemanja Malesija",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/nemanja_malesija.jpeg"
+            },
+            "liked": false
+        },
+        {
+            "post_id": "8e43b67c-558c-4a7a-936d-c32f762f228b",
+            "user_id": "0a48a2b3-f660-4b20-a261-c9ef15c286f7",
+            "text": "xdvxdvd",
+            "image": null,
+            "audio": null,
+            "comments": 0,
+            "likes": 0,
+            "created_at": "2025-10-25T17:45:32.961Z",
+            "user": {
+                "username": "nemanja_malesija",
+                "full_name": "Nemanja Malesija",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/nemanja_malesija.jpeg"
+            },
+            "liked": false
+        },
+        {
+            "post_id": "84872643-e011-41e6-9534-1af0c4df281d",
+            "user_id": "ec7bb1a4-464d-4a32-84fe-dd31c6a88a6f",
+            "text": "Test",
+            "image": null,
+            "audio": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/be365c7c-23aa-438f-9c5f-0a21cd9fff6e-blob",
+            "comments": 0,
+            "likes": 0,
+            "created_at": "2023-09-20T19:16:39.037Z",
+            "user": {
+                "username": "constelic",
+                "full_name": "John Constelic",
+                "picture": "https://constel-hr-frontend.s3.eu-central-1.amazonaws.com/image+(2).png"
+            },
+            "liked": false
+        }
+    ]
+}
