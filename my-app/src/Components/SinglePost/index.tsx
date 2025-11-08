98109e9f559d06ac30254a54c9e3540a3a0b9dbc
Bernard Krehula <krehula.bernard456@gmail.com>
2025-11-08 21:09:27 +0100

    singlepost data defined

--- my-app/src/Components/SinglePost/index.tsx
+++ my-app/src/Components/SinglePost/index.tsx
@@ -1,6 +1,26 @@
+import './index.css'
 
-const SinglePost = ({data}) => {
-     console.log(data)
+type SinglePostDataType = {
+    data: {
+        post_id: string;
+        user_id: string;
+        text: string;
+        image: string;
+        audio: null;
+        comments: number;
+        likes: number;
+        created_at: string;
+        user: {
+            username: string;
+            full_name: string;
+            picture: string;
+        },
+        liked: boolean;
+    }   
+}
+
+const SinglePost = ({data}: SinglePostDataType) => {
+    console.log(data)
  
     return(
         <div className="single-post">
