f8770414fd8fa6aa03c11382ecd90127347dddef
Bernard Krehula <krehula.bernard456@gmail.com>
2025-11-11 19:27:42 +0100

    variation added to singleinput

--- my-app/src/Components/SingleInput/index.tsx
+++ my-app/src/Components/SingleInput/index.tsx
@@ -5,14 +5,15 @@ type SingleInputType = {
     placeholder: string;
     name: 'email' | 'password';
     register: UseFormRegister<FormInputTypes>;
+    variation: string;
 }
 export type FormInputTypes = {
     email: string;
     password: string;
 }
 
-const SingleInput = ({placeholder, name, register}: SingleInputType) => {
-    if(register) return <input className={`single-input`} placeholder={placeholder} {...register(name)}/>
+const SingleInput = ({variation, placeholder, name, register}: SingleInputType) => {
+    if(register) return <input className={`single-input ${variation}`} placeholder={placeholder} {...register(name)}/>
     else return <input className={`single-input`} placeholder={placeholder}/>
 }
 
