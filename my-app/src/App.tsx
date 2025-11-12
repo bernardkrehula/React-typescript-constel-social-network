004215efdcf1a9ecc6e4c7fbfe665f7c33c0a713
Bernard Krehula <krehula.bernard456@gmail.com>
2025-11-12 19:59:50 +0100

    return app tsx with login and homepage component

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Login from "./Pages/Login";
+import Homepage from "./Pages/Homepage";
+
+const App = () => {
+
+    return(
+        <div className="main">
+            <Homepage />
+            <Login />
+        </div>
+    )
+}
+export default App;
