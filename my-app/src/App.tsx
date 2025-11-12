724749d99ee355be723f7b8998be3a8d8ec20586
Bernard Krehula <krehula.bernard456@gmail.com>
2025-11-12 20:05:21 +0100

    login data use state added in app tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,12 @@
 import Login from "./Pages/Login";
 import Homepage from "./Pages/Homepage";
+import { useState } from "react";
 
 const App = () => {
+    const [loginData, setLoginData] = useState({
+        status: '',
+        token: ''
+    });
 
     return(
         <div className="main">
