25538e6b66b2670ae4b88e800a24bb1f6e92f7fc
Bernard Krehula <krehula.bernard456@gmail.com>
2025-11-19 19:48:47 +0100

    input type added

--- my-app/src/Components/SingleInput/index.tsx
+++ my-app/src/Components/SingleInput/index.tsx
@@ -6,21 +6,22 @@ type SingleInputType = {
     name: 'email' | 'password';
     register: UseFormRegister<FormInputTypes>;
     variation: string;
+    type: string;
 }
 export type FormInputTypes = {
     email: string;
     password: string;
 }
 //How to include prop conditionaly in react 
-const SingleInput = ({variation, placeholder, name, register}: SingleInputType) => {
+const SingleInput = ({variation, placeholder, name, type, register}: SingleInputType) => {
     /*  
     const extraconfig = { 
         placeholder: placeholder, 
         ...{ register? ...register(name): {} }
     }
     */
-    if(register) return <input className={`single-input ${variation}`} placeholder={placeholder} {...register(name)}/>
-    else return <input className={`single-input ${variation}`} placeholder={placeholder}/>
+    if(register) return <input className={`single-input ${variation}`} type={type} placeholder={placeholder} {...register(name)}/>
+    else return <input className={`single-input ${variation}`} type={type} placeholder={placeholder}/>
 }
 
 export default SingleInput;
