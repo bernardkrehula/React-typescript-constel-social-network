2a3013f34de726af422a356a7952231e07f8a68b
Bernard Krehula <krehula.bernard456@gmail.com>
2025-12-19 19:51:31 +0100

    changeCommentValue added

--- src/Components/CommentCreator/index.tsx
+++ src/Components/CommentCreator/index.tsx
@@ -1,16 +1,25 @@
 import './index.css'
 import SingleInput from '../SingleInput';
 import Btn from '../Btn';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const CommentCreator = () => {
     const [commentValue, setCommentValue] = useState<string>('');
 
+    const changeCommentValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setCommentValue(value);
+    }
+
+    const passCommentValueToParent = () => {
+
+    }
+
     return(
         <div className='comment-creator'>
             <img src="/user-logo.jpg" />
-            <SingleInput name='comment-creator' type='text' placeholder="What's on your mind?" variation='input--mid'/>
-            <Btn variation='primary--mid' type='button'>Add comment</Btn>
+            <SingleInput name='comment-creator' onChange={changeCommentValue} type='text' placeholder="What's on your mind?" variation='input--mid'/>
+            <Btn variation='primary--mid' onClick={passCommentValueToParent} type='button'>Add comment</Btn>
         </div>
     )
 }
